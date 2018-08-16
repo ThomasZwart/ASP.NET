cc7f6cfad1ff0e7b2abed220fa0f3b6a291f796c
ThomasZwart <36655280+ThomasZwart@users.noreply.github.com>
2018-08-16 18:10:03 +0200

    Initial Commit

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -10,6 +10,21 @@ namespace Vidly {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapMvcAttributeRoutes();
+
+            // Eigen route maken
+            // Specifiekere route moet boven en meer generic onder
+            // Convention based routing (The old way)
+            /*routes.MapRoute(
+                "Movies",
+                "movie/released/{year}/{month}",
+                // default
+                new { controller = "Movie", action = "ByReleaseDate" },
+                // Regular expressions for constraints on what can be in de URL
+                new { year = @"\d{4}", month = @"\d{2}" });
+            */
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+
+
+            // When invoking view(model) the view method assigns the model (movie in this case) to the 'model' property in the dictionary viewData
+            var viewResult = new ViewResult();
+            var model = viewResult.ViewData.Model;
+
+            return View(movie);
+            //return Content("<h2>hi</hi>");
+            //return HttpNotFound(); 
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy ="name" });
+        }
+
+        // Moet 'id' heten om met URL een parameter mee te geven, want zo heet t in de router
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        // Optional parameters, int? is now nullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy)) {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}$sortBy{1}", pageIndex, sortBy));
+        }
+
+        [Route("movie/released/{year:regex(^\\d{4}$)}/{month:regex(^\\d{2}$):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+}
